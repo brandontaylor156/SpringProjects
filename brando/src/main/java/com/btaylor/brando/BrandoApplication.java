d5e732e6566a75819da307b87af38f742f5e1deb
brandontaylor156 <brandon.david4@outlook.com>
2022-08-08 12:57:56 -0700

    changes

---
+++ brando/src/main/java/com/btaylor/brando/BrandoApplication.java
@@ -0,0 +1,13 @@
+package com.btaylor.brando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandoApplication.class, args);
+	}
+
+}
---
+++ brando/src/test/java/com/btaylor/brando/BrandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btaylor.brando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
