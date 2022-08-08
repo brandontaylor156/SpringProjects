c9ccf5194fd8448ff5c48ed8e6edf9bb0e4dd4fb
brandontaylor156 <brandon.david4@outlook.com>
2022-08-08 12:55:49 -0700

    Books API

---
+++ mvc/src/main/java/com/btaylor/mvc/services/BookService.java
@@ -0,0 +1,47 @@
+package com.btaylor.mvc.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import com.btaylor.mvc.models.Book;
+import com.btaylor.mvc.repositories.BookRepository;
+
+@Service
+public class BookService {
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    
+    public Book updateBook(Long id, String title, String desc, String lang, int numOfPages) {
+    	Book bookToUpdate = findBook(id);
+    	bookToUpdate.setTitle(title);
+    	bookToUpdate.setDescription(desc);
+    	bookToUpdate.setLanguage(lang);
+    	bookToUpdate.setNumberOfPages(numOfPages);
+    	return bookRepository.save(bookToUpdate);
+    }
+    
+    public void deleteBook(Long id) {
+    	bookRepository.deleteById(id);
+    }
+}
---
+++ mvc/src/test/java/com/btaylor/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btaylor.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
